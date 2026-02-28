3df29713efe6ea05173329b4ac4e60970276be06
Abishek <abishek@Abisheks-MacBook-Air.local>
2026-02-28 14:48:42 +0530

    fix: delay MONGODB_URI check until actual connection call to fix build

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,10 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI!;
-
-if (!MONGODB_URI) {
-    throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
-}
+const MONGODB_URI = process.env.MONGODB_URI;
 
 // Global caching for connections
 interface MongooseCache {
@@ -32,11 +28,15 @@ async function connectToDatabase() {
     }
 
     if (!cached!.promise) {
+        if (!process.env.MONGODB_URI) {
+            throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
+        }
+
         const opts = {
             bufferCommands: false,
         };
 
-        cached!.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+        cached!.promise = mongoose.connect(process.env.MONGODB_URI, opts).then((mongoose) => {
             return mongoose;
         });
     }
